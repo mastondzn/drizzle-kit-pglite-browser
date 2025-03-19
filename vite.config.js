b24a653fd5701a4026efbbd11ac11e64c6175d68
Maston <22679886+mastondzn@users.noreply.github.com>
2025-03-19 20:20:06 +0200

    fix: fix pages path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
   },
+  base: process.env.GITHUB_ACTIONS ? "/drizzle-kit-pglite-browser/" : "/",
   plugins: [tailwindcss()],
 });
